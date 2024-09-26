8f6c24b5715de30f550f65127fd5a8f83fb3b8d7
armales <armansk8er@gmail.com>
2024-09-26 14:36:05 +0500

    Упражнение верстка статьи

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,38 @@ function App() {
 
   return (
     <>
-      <a href="http://">Текст ссылки</a><br />
-      <a href="http://wikipedia.org" target="_blank">Ссылка в новом окне</a><br />
-      <a href="http://wikipedia.org" target="_parent">Ссылка в родительском фрейме</a><br />
-      <a href="http://wikipedia.org" target="_top">Ссылка в полном окне</a><br />
-      <a href="https://example.com" target="_blank" rel="noopener noreferrer">Открыть с защитой</a><br />
-      <a href="example.pdf" download="Инструкция.pdf">Скачать инструкцию</a><br />
+    <div className='container'>
+      <h1>Добро пожаловать</h1>
+      <h2>Как заработать миллионы на лимонах</h2>
+      <h4>Содержание</h4>
+      <ol>
+        <li><strong>Lorem, ipsum.</strong></li>
+        <ul>
+          <li>Lorem, ipsum dolor.</li>
+          <li>Praesentium, exercitationem magni.</li>
+          <li>Ab, sapiente excepturi?</li>
+        </ul>
+        <li><strong>Lorem, ipsum.</strong></li>
+        <ul>
+          <li>Lorem, ipsum dolor.</li>
+          <li>Praesentium, velit temporibus.</li>
+          <li>Consequuntur, ipsa est!</li>
+        </ul>
+        <li><strong>Lorem, ipsum.</strong></li>
+        <ul>
+          <li>Lorem, ipsum dolor.</li>
+          <li>Nostrum, eaque id.</li>
+          <li>Eius, architecto voluptate!</li>
+        </ul>
+      </ol>
+      <br />
+      <h4>1. Lorem, ipsum.</h4>
+      <p><span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur totam dolorem provident, quas quidem, quasi, incidunt atque minus maxime eveniet laborum sunt recusandae temporibus quos. Facilis quasi molestiae accusantium ratione!</span><span>Nemo ab accusamus facilis assumenda eum debitis dolor at est cum molestias qui totam dolore soluta sunt vel, perspiciatis in quidem alias corrupti adipisci sequi neque delectus impedit quasi. In?</span><span>Excepturi deleniti animi ex minima voluptatibus maxime voluptates vero labore veniam! Temporibus laborum, tempora odio hic quia voluptates tempore dicta quas placeat necessitatibus sit velit, eum facilis nostrum fugiat dolor.</span></p>
+      <h4>2. Repudiandae, ducimus?</h4>
+      <p><span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate consectetur pariatur dolore natus dolor velit nisi, amet porro, maiores iste doloremque mollitia incidunt nostrum earum voluptatum fugit et fuga adipisci.</span><span>Distinctio dicta nulla labore, obcaecati minima natus, unde est provident pariatur possimus error quos iusto officia architecto incidunt omnis, qui blanditiis? Explicabo, repudiandae commodi ipsa mollitia asperiores dolore voluptatum rerum?</span><span>Cum maiores veniam ratione. Maiores dignissimos, accusantium consequuntur ratione cupiditate quas sequi itaque, fugiat ipsa quidem eum quia ab reprehenderit minus dolores exercitationem quam! Animi dignissimos sequi mollitia maiores repellat!</span></p>
+      <h4>3. Totam, voluptas.</h4>
+      <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque architecto quasi sunt eveniet facilis aut illum optio non vel id neque magnam dicta, voluptatibus dolorem quis assumenda vitae aliquid ipsam!</span><span>Nesciunt eaque praesentium expedita facilis fuga exercitationem consequatur minus, veritatis, delectus odit ab nulla quo recusandae sapiente doloribus itaque suscipit provident. Reiciendis fuga ab dignissimos eum consectetur saepe excepturi quae.</span><span>Accusamus voluptates, deserunt nesciunt cum vero numquam ducimus dolore eius ipsa, quae in, repellat eaque. Iusto provident sint quia maiores. Quod repellat labore voluptatibus voluptatum vel soluta perferendis eveniet distinctio?</span></p>
+    </div>
     </>
   )
 }
